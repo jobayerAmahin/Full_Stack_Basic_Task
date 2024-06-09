119886e4ba23dd1cdfd2802ebd526bb3f38e439e
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-06-09 23:36:22 +0300

    Practicing JS product and insta profile

---
+++ main.js
@@ -0,0 +1,15 @@
+const product= {
+    otsikko:"Kynä",
+    hinta: 2,
+    luokitus: 4,
+    tarjous: "6%",
+};
+console.log(product);
+
+const instaUser= {
+    henkilö:"General",
+    seuraajat: 276,
+    aktiivi: true,
+};
+console.log(product);
+
